d6e32ea84e082cac3ec95b9274c97b9dcea3a151
Amman <ammankumar777@gmail.com>
2025-02-06 09:10:03 +0530

    redirect check

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,67 @@
+// 'use client'
+
+// import WebApp from '@twa-dev/sdk'
+// import { useEffect, useState } from 'react'
+
+// // Define the interface for user data
+// interface UserData {
+//   id: number;
+//   first_name: string;
+//   last_name?: string;
+//   username?: string;
+//   language_code: string;
+//   is_premium?: boolean;
+// }
+
+// export default function Home() {
+//   const [userData, setUserData] = useState<UserData | null>(null)
+
+//   useEffect(() => {
+//     if (WebApp.initDataUnsafe.user) {
+//       setUserData(WebApp.initDataUnsafe.user as UserData)
+//     }
+//   }, [])
+
+//   return (
+//     <main className="p-4">
+//       {userData ? (
+//         <>
+//           <h1 className="text-2xl font-bold mb-4">User Data</h1>
+//           <ul>
+//             <li>ID: {userData.id}</li>
+//             <li>First Name: {userData.first_name}</li>
+//             <li>Last Name: {userData.last_name || 'N/A'}</li>
+//             <li>Username: {userData.username || 'N/A'}</li>
+//             <li>Language Code: {userData.language_code}</li>
+//             <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+//           </ul>
+//         </>
+//       ) : (
+//         <div>Working...</div>
+//       )}
+//     </main>
+//   )
+// }
+
+
+
+
+// redirect
 'use client'
 
-import WebApp from '@twa-dev/sdk'
-import { useEffect, useState } from 'react'
-
-// Define the interface for user data
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: boolean;
-}
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const [userData, setUserData] = useState<UserData | null>(null)
+  const router = useRouter()
 
   useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      setUserData(WebApp.initDataUnsafe.user as UserData)
-    }
+    window.location.href = "https://t.me/+wS5_mahZWWk3OTI1"
   }, [])
 
   return (
     <main className="p-4">
-      {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Working...</div>
-      )}
+      <div>Redirecting...</div>
     </main>
   )
 }
